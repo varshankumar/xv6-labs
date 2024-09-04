20b0b57d5fe818f51b85af57b6060e03c6cb7d19
Varshan <41884234+varshankumar@users.noreply.github.com>
2024-09-03 18:52:25 -0700

    Update sleep.c

--- user/sleep.c
+++ user/sleep.c
@@ -1,11 +1,12 @@
 #include<unistd.h>
-#include<stdlib.h>
-#include<stdio.h>
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
 
 int main(int argc, char *argv[]) {
 	if (argc > 1)
 		sleep(atoi(argv[1]));
 	else
-		printf("Argument not given\n");
+		write("Argument not given\n");
 	exit(0);
 }
